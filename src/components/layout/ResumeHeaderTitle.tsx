f3c435a4c2d324d8d1b3f7bbb1e2664b44fb8630
Anil Kumar <anil14349@gmail.com>
2025-05-02 00:14:39 +0530

    added X mark css

--- src/components/layout/ResumeHeaderTitle.tsx
+++ src/components/layout/ResumeHeaderTitle.tsx
@@ -53,11 +53,11 @@ const ResumeHeaderTitle: React.FC<ResumeHeaderTitleProps> = ({ fontOptions, titl
                         <button
                             type="button"
                             aria-label="Remove title"
-                            className="ml-2 p-1 rounded-full hover:bg-gray-200 transition-colors flex items-center justify-center text-gray-500 text-base h-6 w-6 opacity-0 group-hover:opacity-100"
+                            className="text-gray-400 hover:text-gray-600 dark:hover:text-gray-200 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 ml-2 p-1 rounded-full flex items-center justify-center h-6 w-6"
                             style={{ lineHeight: 1, minHeight: 0, marginBottom: 0, pointerEvents: hovered ? 'auto' : 'none' }}
                             onClick={onDelete}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="16" height="16">
+                            <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M10 8.586l4.95-4.95a1 1 0 111.414 1.414L11.414 10l4.95 4.95a1 1 0 01-1.414 1.414L10 11.414l-4.95 4.95a1 1 0 01-1.414-1.414l4.95-4.95-4.95-4.95A1 1 0 015.05 3.636L10 8.586z" clipRule="evenodd" />
                             </svg>
                         </button>
